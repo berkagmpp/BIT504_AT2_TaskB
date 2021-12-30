75565edad709ace9e24694f38687df00dfa0593a
Claire Ahn <berkagmpp@gmail.com>
2021-12-30 13:28:42 +1300

    Add dot

--- src/com/bit504/at2/PongPanel.java
+++ src/com/bit504/at2/PongPanel.java
@@ -1,5 +1,6 @@
 package com.bit504.at2;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,6 +11,7 @@ import javax.swing.Timer;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
@@ -47,7 +49,15 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.WHITE);
-		g.fillRect(20, 20, 100, 100);
+		paintDottedLine(g);
+	}
+
+	private void paintDottedLine(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g.create();
+		Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 9 }, 0);
+		g2d.setStroke(dashed);
+		g2d.setPaint(Color.WHITE);
+		g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+		g2d.dispose();
 	}
 }
