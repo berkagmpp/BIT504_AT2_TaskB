41076dbec84a85565243ecb8dd5a2f0d26e97a81
Claire Ahn <berkagmpp@gmail.com>
2021-12-30 13:20:45 +1300

    Add Pong class

--- src/com/bit504/at2/Pong.java
+++ src/com/bit504/at2/Pong.java
@@ -1,9 +1,19 @@
 package com.bit504.at2;
 
-public class Pong {
+import javax.swing.JFrame;
+
+public class Pong extends JFrame {
+
+	public Pong() {
+		setTitle("Pong");
+		setSize(800, 600);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		new Pong();
 	}
 
 }
