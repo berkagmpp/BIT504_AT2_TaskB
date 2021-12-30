90fb71cb789d64ce5674e3dbfc41849671c64455
Claire Ahn <berkagmpp@gmail.com>
2021-12-30 13:27:44 +1300

    Add update and paintComponent methods

--- src/com/bit504/at2/PongPanel.java
+++ src/com/bit504/at2/PongPanel.java
@@ -6,13 +6,20 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
 
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.start();
 	}
 
 	@Override
@@ -27,8 +34,20 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	public void keyTyped(KeyEvent event) {
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent event) {
+	private void update() {
+
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		update();
+		repaint();
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
+	}
 }
